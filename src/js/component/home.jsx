bc1976c993d459b154307247433e9bb3f0ed4240
RdeRausseo <amrausseo20@gmail.com>
2024-11-18 20:38:37 +0000

    realizando bonus

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,19 +1,37 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState} from "react";
 import Circle from "./circulo";
 
 //create your first component
 const Home = () => {
+
+	
+	const [color, setColor] = useState("red");
+	
+	function changeColor(color){
+		switch(color){
+			case "green":
+				return "yellow";
+			case "yellow":
+				return "red";
+			case "red":
+				return "green";
+		}
+	}
+
 	return (
 		<>
 			<div className="palo"></div>
 			<div className="container">
-				<Circle className="circle"/>
-				<Circle className="circle"/>
-				<Circle className="circle"/>
+				<Circle onClick={() => setColor("green")} focus={color==="green"? "focus":""} fondo="green"/>
+				<Circle onClick={() => setColor("yellow")} focus={color==="yellow"? "focus":""} fondo="yellow"/>
+				<Circle onClick={() => setColor("red")} focus={color === "red"? "focus":""} fondo="red"/>
+				{color==="blueviolet" &&
+					<Circle onClick={() => setColor("blueviolet")} focus={color === "blueviolet"? "focus":""} fondo="blueviolet"/>}
+
 			</div>
+	
+				<button onClick={() => setColor(changeColor(color))} type="button" className="d-block my-2 mx-auto btn btn-outline-dark">change color</button>
+				<button onClick={() => setColor("blueviolet")}  type="button" className="d-block mx-auto btn btn-outline-primary"> Add the Purple Color </button>
 		</>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom/client";
+import 'bootstrap/dist/css/bootstrap.css';
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
